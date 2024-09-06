f005ddb9302550435cc6bcf70ecfb9ff4b6bf344
SILAMEAS <sila.meas@allweb.com.kh>
2024-09-06 16:20:15 +0700

    first commit

---
+++ src/main/java/com/sila/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package com.sila.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class AccessDeniedException extends RuntimeException{
+    public AccessDeniedException(String message){
+        super(message);
+    }
+}
